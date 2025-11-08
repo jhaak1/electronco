40cb753de99ddfeb21626f7d59d5415c95cafac9
Jeremy Haakenson <jkhaak@gmail.com>
2025-11-07 16:13:37 -0800

    -

--- R/script.R
+++ R/script.R
@@ -1,8 +1,8 @@
 library('readr')
 
-icd9 = read_csv('icd9.csv')
-icd10 = read_csv('icd10.csv')
-cpt = read_csv('cpt.csv')
+#icd9 = read_csv('icd9.csv')
+#icd10 = read_csv('icd10.csv')
+#cpt = read_csv('cpt.csv')
 
 usethis::use_data(icd9, icd10, cpt, internal = T)
 usethis::use_data_raw()
@@ -17,3 +17,7 @@ check()
 use_package('tibble')
 use_package('yaml')
 document()
+
+profvis({
+
+})
