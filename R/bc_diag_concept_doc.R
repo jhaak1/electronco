098b89a9d856c02a5b9e63f46a5973a659d76561
Jeremy Haakenson <jkhaak@gmail.com>
2025-11-10 16:15:23 -0800

    -

---
+++ R/bc_diag_concept_doc.R
@@ -0,0 +1,16 @@
+#' Breast Cancer Diagnosis Concept Set.
+#'
+#' A data frame of diagnosis codes used to identify breast cancer in EHRs.
+#'
+#' @format A data frame with \code{n} rows and the following columns:
+#' \describe{
+#'   \item{code}{character. code string.}
+#'   \item{code_system}{character. vocabulary name (e.g., "icd10").}
+#'   \item{include}{logical. T for included codes, F for explicit exclusions.}
+#' }
+#' @source Created for the electronco package.
+#' @examples
+#' head(bc_diag_concept)
+#' @name bc_diag_concept
+#' @docType data
+NULL
