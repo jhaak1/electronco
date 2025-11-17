3d5522025b75b000dff722942383aef34ead0888
Jeremy Haakenson <jkhaak@gmail.com>
2025-11-17 14:23:44 -0800

    -

--- R/utils.R
+++ R/utils.R
@@ -4,6 +4,7 @@ if (getRversion() >= "2.15.1") {
     "code", "bc_diag_concept", "patient_id", "n_total", "meets_min_event",
     "first_date", "last_date", "diagnosis_flag", "is_canonical",
     "bc_proc_concept", ".data", ".proc_code_raw", "code_norm", "group",
-    "cohort_name", "cohort_count", "cohort_start"
+    "cohort_name", "cohort_count", "cohort_start", ".order_date_parsed",
+    ".med_name_norm", "drug_matched", "cohort_flag"
   ))
 }
